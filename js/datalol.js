96305bc87bfcb0cd950992a3a977a41320e58902
athorel <alexis.thorel@gmail.com>
2016-07-01 20:18:06 +0200

    chart stats most played champions

--- js/datalol.js
+++ js/datalol.js
@@ -9,11 +9,13 @@ $(document).ready(function() {
 			var summonerId = response[summonerName].id;
 			drawChartLastMatches(summonerId);
 			getMostPlayedChampions(summonerId);
+			getStatsSummoner(summonerId);
 		});
 	});
 
 	function drawChartLastMatches(summonerId) {
 		$.get(urlRiotApi + "v1.3/game/by-summoner/" + summonerId + "/recent?api_key=" + apiKey, function(response) {
+			console.log(response);
 			var lastGames = response.games,
 				gamesWon = 0,
 				gamesLose = 0;
@@ -72,21 +74,91 @@ $(document).ready(function() {
 				return 0; 
 			} 
 			championsPlayed.sort(compare);
-
-			var idFirstChampion = championsPlayed[1].id,
-				idSecondChampion = championsPlayed[2].id,
-				idThirdChampion = championsPlayed[3].id;
 			
 			var tempI = 1;
-			for(var i = 1; i <= 3 ; i++) {
+			var data = [{},{},{},{},{}];
+			for(var i = 1; i <= 5 ; i++) {
+				data[i - 1].totalGamesPlayed = championsPlayed[i].stats.totalSessionsPlayed;
+				data[i - 1].totalGamesLost = championsPlayed[i].stats.totalSessionsLost;
+				data[i - 1].totalGamesWon = championsPlayed[i].stats.totalSessionsWon;
 				$.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/" + championsPlayed[i].id + "?api_key=" + apiKey + "&champData=all", function(response) {
-					var championName = response.key;
-					var img = document.createElement("img");
-					img.setAttribute("src", "http://ddragon.leagueoflegends.com/cdn/6.13.1/img/champion/" + championName + ".png");
-					document.getElementById("champion-" + tempI).appendChild(img);
+					data[tempI - 1].name = response.name;
+					data[tempI - 1].img = "http://ddragon.leagueoflegends.com/cdn/6.13.1/img/champion/" + response.key + ".png";
 					tempI++;
+					if(tempI == 6) {
+						drawChartMostPlayedChampions(data);
+					}
 				});
 			}
 		});
+	}
+
+	function getStatsSummoner(summonerId) {
+		// /api/lol/{region}/v1.3/stats/by-summoner/{summonerId}/summary
+	}
+
+	function drawChartMostPlayedChampions(data) {
+		var chart = $('#chartMostPlayedChampions').highcharts({
+	        chart: {
+	            type: 'column'
+	        },
+	        title: {
+	            text: ' '
+	        },
+	        xAxis: {
+	            categories: [
+	                data[0].name,
+	                data[1].name,
+	                data[2].name,
+	                data[3].name,
+	                data[4].name
+	            ],
+	            labels: {
+                    color: '#fff',
+                    x: 5,
+                    useHTML: true,
+                    formatter: function () {
+                        var img = this.value.toLowerCase();
+                        img = img.charAt(0).toUpperCase() + img.slice(1);
+                        return '<img class="champion" src="http://ddragon.leagueoflegends.com/cdn/6.13.1/img/champion/' + img + '.png" />';
+                    }
+                },
+	            crosshair: true
+	        },
+	        yAxis: {
+	            min: 0,
+	            title: {
+	                text: 'Number of games played'
+	            }
+	        },
+	        legend: {
+	            verticalAlign: 'top'	
+	        },
+	        tooltip: {
+	            headerFormat: '<table>',
+	            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+	                '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+	            footerFormat: '</table>',
+	            shared: true,
+	            useHTML: true
+	        },
+	        plotOptions: {
+	            column: {
+	                pointPadding: 0.2,
+	                borderWidth: 0,
+	                verticalAlign: "top"
+	            },
+	        },
+	        series: [{
+	            name: 'Total of games played',
+	            data: [data[0].totalGamesPlayed, data[1].totalGamesPlayed, data[2].totalGamesPlayed, data[3].totalGamesPlayed, data[4].totalGamesPlayed]
+	        }, {
+	            name: 'Games won',
+	            data: [data[0].totalGamesWon, data[1].totalGamesWon, data[2].totalGamesWon, data[3].totalGamesWon, data[4].totalGamesWon]
+	        }, {
+	            name: 'Games lose',
+	            data: [data[0].totalGamesLost, data[1].totalGamesLost, data[2].totalGamesLost, data[3].totalGamesLost, data[4].totalGamesLost]
+	        }]
+	    });
 	}
 });
